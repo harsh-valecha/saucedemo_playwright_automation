478899d5752bab5658e85da17811f309ef7251cb
Harsh <harsh.valecha@yahoo.com>
2024-10-03 18:16:11 +0530

    added a line to remember how to debug testcases in playwright

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,4 +12,5 @@ def test_invalid_login(page):
     login_page = LoginPage(page, 'https://www.saucedemo.com/')
     login_page.load_page()
     login_page.login('test_user', 'secret_sauce')
+    #page.pause() # adding breakpoint to debug the testcase
     assert login_page.get_error() == 'Epic sadface: Username and password do not match any user in this service'
