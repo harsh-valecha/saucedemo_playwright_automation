2551afe963164c81003144c0c37902d73559f270
Harsh <harsh.valecha@yahoo.com>
2024-10-04 21:38:09 +0530

    updated urls to work properly using Config class

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,10 +1,11 @@
 import pytest
 from playwright.sync_api import Page
 from pages.inventory_page import InventoryPage
+from utils.config import Config
 
 @pytest.mark.order(2)
 def test_add_to_cart(page):
-    inventory_page = InventoryPage(page,'https://www.saucedemo.com/inventory.html')
+    inventory_page = InventoryPage(page,Config.inventory_page_url)
     inventory_page.add_to_cart_button.click()
     assert inventory_page.add_to_cart_button.is_visible()==False
     assert inventory_page.remove_button.is_visible()==True
@@ -13,7 +14,7 @@ def test_add_to_cart(page):
 #@pytest.mark.dependency(depends=["test_add_to_cart"])
 @pytest.mark.order(3)
 def test_empty_cart(page):
-    inventory_page = InventoryPage(page,'https://www.saucedemo.com/inventory.html')
+    inventory_page = InventoryPage(page,Config.inventory_page_url)
     assert inventory_page.remove_button.is_visible()==True
     inventory_page.remove_button.click()
     assert inventory_page.add_to_cart_button.is_visible()==True
@@ -21,7 +22,7 @@ def test_empty_cart(page):
 
 @pytest.mark.order(4)
 def test_sort_titles_ascending(page):
-    inventory_page = InventoryPage(page,'https://www.saucedemo.com/inventory.html')
+    inventory_page = InventoryPage(page,Config.inventory_page_url)
     titles = inventory_page.inventory_titles.all_inner_texts()
     titles.sort()
     inventory_page.sorter_dropdown.select_option(value='az')
@@ -31,7 +32,7 @@ def test_sort_titles_ascending(page):
 
 @pytest.mark.order(5)
 def test_sort_titles_descending(page):
-    inventory_page = InventoryPage(page, 'https://www.saucedemo.com/inventory.html')
+    inventory_page = InventoryPage(page, Config.inventory_page_url)
     titles = inventory_page.inventory_titles.all_inner_texts()
     titles.sort(reverse=True)
     inventory_page.sorter_dropdown.select_option(value='za')
@@ -41,7 +42,7 @@ def test_sort_titles_descending(page):
 
 @pytest.mark.order(6)
 def test_sort_prices_ascending(page):
-    inventory_page = InventoryPage(page, 'https://www.saucedemo.com/inventory.html')
+    inventory_page = InventoryPage(page, Config.inventory_page_url)
     prices = inventory_page.inventory_prices.all_text_contents()
     prices_sorted = [float(i[1:]) for i in prices ]
     prices_sorted.sort()
@@ -53,7 +54,7 @@ def test_sort_prices_ascending(page):
 
 @pytest.mark.order(7)
 def test_sort_prices_descending(page):
-    inventory_page = InventoryPage(page, 'https://www.saucedemo.com/inventory.html')
+    inventory_page = InventoryPage(page, Config.inventory_page_url)
     prices = inventory_page.inventory_prices.all_text_contents()
     prices_sorted = [float(i[1:]) for i in prices ]
     prices_sorted.sort(reverse=True)
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,17 +1,21 @@
 from lib2to3.fixes.fix_input import context
 import pytest
 from pages.login_page import LoginPage
+from utils.config import Config
+from data.db_connector import get_users
 
+@pytest.mark.parametrize("user_data",get_users())
 @pytest.mark.order(1)
-def test_valid_login(page):
-    login_page = LoginPage(page,'https://www.saucedemo.com/')
-    login_page.login('standard_user','secret_sauce')
+def test_valid_login(page,user_data:dict):
+    login_page = LoginPage(page,Config.login_page_url)
+    login_page.login(user_data['username'],user_data['password'])
+    print(user_data['username'])
     assert page.url == 'https://www.saucedemo.com/inventory.html'
 
 
 @pytest.mark.skip
 def test_invalid_login(page):
-    login_page = LoginPage(page, 'https://www.saucedemo.com/')
+    login_page = LoginPage(page, Config.login_page_url)
     login_page.login('test_user', 'secret_sauce')
     #page.pause() # adding breakpoint to debug the testcase
     assert login_page.get_error() == 'Epic sadface: Username and password do not match any user in this service'
